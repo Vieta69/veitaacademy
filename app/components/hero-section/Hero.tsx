3fb7296aacfbb7213355b36a385f08166a622c67
Vieta69 <vieta.academy@gmail.com>
2025-01-22 21:57:29 +0530

    Update Hero.tsx

--- app/components/hero-section/Hero.tsx
+++ app/components/hero-section/Hero.tsx
@@ -92,7 +92,7 @@ export default function Hero() {
           <span className="bg-gradient-to-br bg-clip-text text-transparent from-[#7CC0C4] via-[#548FBA] to-[#3C84C7]">
             Vieta Academy
           </span>
-          <p>Connecting Brillian Minds</p>
+          <p>Connecting</p>
         </motion.h1>
 
         <motion.p
