bf35f18367096fad94412d4b84c4593128812367
Vieta69 <vieta.academy@gmail.com>
2025-01-27 11:16:14 +0530

    Update Works.tsx

--- app/components/work-section/Works.tsx
+++ app/components/work-section/Works.tsx
@@ -13,32 +13,15 @@ export default function Works() {
 
   const works = [
     {
-      title: "Offset Africa",
+      title: "Price Tag",
       // gitLink: "https://github.com/adex-hub/url-shortening-api-landing",
-      liveLink: "https://offsetcomms.africa/",
+      liveLink: "",
+        gitLink: "",
       about:
-        "Offset Communications Africa’s website really allows them to stand out in the PR sector. It shows their works and case studies — highlighting their expertise and educating users about the company philosophy.",
+        "Details about the course here!",
       stack: ["next.js", "typescript", "framer motion", "tailwindcss"],
-      img: "/offset-folio.svg",
     },
-    {
-      title: "Shortly",
-      gitLink: "https://github.com/adex-hub/url-shortening-api-landing",
-      liveLink: "https://lnk-shortener.netlify.app/",
-      about:
-        "A link shortener for a fictional company that effectively shortens links and stores them. No matter how many links you shorten, you can revisit the site anytime to view and manage your previously shortened links.",
-      stack: ["next.js", "typescript", "framer motion", "tailwindcss"],
-      img: "/shortly.svg",
-    },
-    {
-      title: "Where in the world",
-      gitLink: "https://github.com/adex-hub/where-in-the-world",
-      liveLink: "https://where-in-the-world-ade.netlify.app/",
-      about:
-        "A fun tool for checking information about any country in the world such as the country’s native name, currency, population,  border countries etc. It features dynamic routing for each country and theme switch.",
-      stack: ["react", "react-router", "javascript", "tailwindcss"],
-      img: "/witw.svg",
-    },
+    
     // {
     //   title: "Yourtodo",
     //   gitLink: "https://github.com/adex-hub/Yourtodo",
@@ -48,15 +31,15 @@ export default function Works() {
     //   stack: ["react", "javascript", "figma", "sass"],
     //   img: "/todo.svg",
     // },
-    {
-      title: "Multi-step form",
-      gitLink: "https://github.com/adex-hub/multi-step-form-main",
-      liveLink: "https://ade-loremgaming.netlify.app/",
-      about:
-        "This site features a clean, user-friendly multi-step form for subscribing to a gaming service. Users input personal info, select a plan (monthly or yearly), choose add-ons (online service, larger storage, customizable profile), and review the summary before confirming.",
-      stack: ["html", "sass", "javascript", "ms clarity"],
-      img: "/multistep.svg",
-    },
+    // {
+    //   title: "Multi-step form",
+    //   gitLink: "https://github.com/adex-hub/multi-step-form-main",
+    //   liveLink: "https://ade-loremgaming.netlify.app/",
+    //   about:
+    //     "This site features a clean, user-friendly multi-step form for subscribing to a gaming service. Users input personal info, select a plan (monthly or yearly), choose add-ons (online service, larger storage, customizable profile), and review the summary before confirming.",
+    //   stack: ["html", "sass", "javascript", "ms clarity"],
+    //   img: "/multistep.svg",
+    // },
   ];
 
   const { ref, inView } = useInView({
