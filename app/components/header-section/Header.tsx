9d89b31bbeac9d828e7d42c1985521aae6c2b0d0
Vieta69 <vieta.academy@gmail.com>
2025-01-29 16:43:00 +0530

    Update Header.tsx

--- app/components/header-section/Header.tsx
+++ app/components/header-section/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         <div className="flex justify-between items-center px-6 py-4 rounded-2xl bg-gradient-to-r from-[#d9d9d91f] to-[#7373731f] mt-4 sm:mt-8 std-backdrop-blur">
           <Image
             src="/Untitled (1).svg"
-              width={32}
+              width={402}
               height={36}
             alt="logo"
             className="select-none"
